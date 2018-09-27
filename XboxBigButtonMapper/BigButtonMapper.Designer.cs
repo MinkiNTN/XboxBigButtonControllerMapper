da7fafb0aa6123bcd7e8b703b304f39a6efd414e
Nhat Nguyen <hugo6644493@gmail.com>
2018-09-27 14:48:59 +0300

    Getting button status from controllers

---
+++ XboxBigButtonMapper/BigButtonMapper.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace XboxBigButtonMapper
+{
+    partial class BigButtonMapper
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.controllerLabel = new System.Windows.Forms.Label();
+            this.buttonLabel = new System.Windows.Forms.Label();
+            this.currentControllerLabel = new System.Windows.Forms.Label();
+            this.currentButtonLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // controllerLabel
+            // 
+            this.controllerLabel.AutoSize = true;
+            this.controllerLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.controllerLabel.Location = new System.Drawing.Point(12, 9);
+            this.controllerLabel.Name = "controllerLabel";
+            this.controllerLabel.Size = new System.Drawing.Size(245, 55);
+            this.controllerLabel.TabIndex = 0;
+            this.controllerLabel.Text = "Controller:";
+            // 
+            // buttonLabel
+            // 
+            this.buttonLabel.AutoSize = true;
+            this.buttonLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonLabel.Location = new System.Drawing.Point(12, 175);
+            this.buttonLabel.Name = "buttonLabel";
+            this.buttonLabel.Size = new System.Drawing.Size(176, 55);
+            this.buttonLabel.TabIndex = 1;
+            this.buttonLabel.Text = "Button:";
+            // 
+            // currentControllerLabel
+            // 
+            this.currentControllerLabel.AutoSize = true;
+            this.currentControllerLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.currentControllerLabel.Location = new System.Drawing.Point(263, 9);
+            this.currentControllerLabel.Name = "currentControllerLabel";
+            this.currentControllerLabel.Size = new System.Drawing.Size(104, 55);
+            this.currentControllerLabel.TabIndex = 2;
+            this.currentControllerLabel.Text = "N/A";
+            // 
+            // currentButtonLabel
+            // 
+            this.currentButtonLabel.AutoSize = true;
+            this.currentButtonLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.currentButtonLabel.Location = new System.Drawing.Point(263, 175);
+            this.currentButtonLabel.Name = "currentButtonLabel";
+            this.currentButtonLabel.Size = new System.Drawing.Size(104, 55);
+            this.currentButtonLabel.TabIndex = 3;
+            this.currentButtonLabel.Text = "N/A";
+            // 
+            // BigButtonMapper
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.currentButtonLabel);
+            this.Controls.Add(this.currentControllerLabel);
+            this.Controls.Add(this.buttonLabel);
+            this.Controls.Add(this.controllerLabel);
+            this.Name = "BigButtonMapper";
+            this.Text = "Xbox Big Button Controller Mapper";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label controllerLabel;
+        private System.Windows.Forms.Label buttonLabel;
+        private System.Windows.Forms.Label currentControllerLabel;
+        private System.Windows.Forms.Label currentButtonLabel;
+    }
+}
+
---
+++ XboxBigButtonMapper/BigButtonMapper.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using XboxBigButton;
+
+namespace XboxBigButtonMapper
+{
+    public partial class BigButtonMapper : Form
+    {
+        /// <summary>
+        /// The big button controller
+        /// </summary>
+        private XboxBigButtonDevice _device;
+
+        /// <summary>
+        /// Time when the last keypress was received from the controller, use this to discard excessive repeating keys
+        /// </summary>
+        private Tuple<Controller, Buttons, DateTime> _lastKeyTime = null;
+
+        public BigButtonMapper()
+        {
+            InitializeComponent();
+
+            _device = new XboxBigButtonDevice();
+            _device.ButtonStateChanged += _device_ButtonStateChanged;
+
+            _device.Connect();
+        }
+
+        private void _device_ButtonStateChanged(object sender, XboxBigButtonDeviceEventArgs e)
+        {
+            if (this.InvokeRequired)
+                this.BeginInvoke(new Action(() => this.SendKeysToWindow(e.Controller, e.ButtonState)));
+            else
+                this.SendKeysToWindow(e.Controller, e.ButtonState);
+        }
+
+        private void SendKeysToWindow(Controller controller, Buttons buttons)
+        {
+            var timeNow = DateTime.Now;
+            if (_lastKeyTime != null &&
+                _lastKeyTime.Item1 == controller &&
+                _lastKeyTime.Item2 == buttons &&
+                (timeNow - _lastKeyTime.Item3).TotalSeconds < .5d)
+            {
+                // If the last key event was within tolerance then don't process it (if key events happen too quickly)
+                return;
+            }
+
+            // Save the time if we successfully sent keys
+            _lastKeyTime = new Tuple<Controller, Buttons, DateTime>(controller, buttons, timeNow);
+
+            switch (controller)
+            {
+                case Controller.Green:
+                    this.currentControllerLabel.Text = "Green";
+                    break;
+                case Controller.Red:
+                    this.currentControllerLabel.Text = "Red";
+                    break;
+                case Controller.Blue:
+                    this.currentControllerLabel.Text = "Blue";
+                    break;
+                case Controller.Yellow:
+                    this.currentControllerLabel.Text = "Yellow";
+                    break;
+                default:
+                    this.currentControllerLabel.Text = "N/A";
+                    break;
+            }
+
+            switch (buttons)
+            {
+                case Buttons.Up:
+                    this.currentButtonLabel.Text = "Up";
+                    break;
+                case Buttons.Down:
+                    this.currentButtonLabel.Text = "Down";
+                    break;
+                case Buttons.Left:
+                    this.currentButtonLabel.Text = "Left";
+                    break;
+                case Buttons.Right:
+                    this.currentButtonLabel.Text = "Right";
+                    break;
+                case Buttons.BigButton:
+                    this.currentButtonLabel.Text = "Big Button";
+                    break;
+                case Buttons.A:
+                    this.currentButtonLabel.Text = "A";
+                    break;
+                case Buttons.B:
+                    this.currentButtonLabel.Text = "B";
+                    break;
+                case Buttons.X:
+                    this.currentButtonLabel.Text = "X";
+                    break;
+                case Buttons.Y:
+                    this.currentButtonLabel.Text = "Y";
+                    break;
+                case Buttons.Back:
+                    this.currentButtonLabel.Text = "Back";
+                    break;
+                case Buttons.Start:
+                    this.currentButtonLabel.Text = "Start";
+                    break;
+                case Buttons.Home:
+                    this.currentButtonLabel.Text = "Home";
+                    break;
+                default:
+                    this.currentButtonLabel.Text = "N/A";
+                    break;
+            }
+
+            //if (buttons.IsPressed(Buttons.Start))
+            //{
+            //    return;
+            //}
+        }
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                components?.Dispose();
+                components = null;
+
+                _device?.Disconnect();
+                _device = null;
+            }
+
+            base.Dispose(disposing);
+        }
+
+        private void Form1_FormClosing(Object sender, FormClosingEventArgs e)
+        {
+            _device?.Disconnect();
+        }
+    }
+}
